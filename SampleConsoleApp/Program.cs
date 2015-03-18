e2641cb95e40766ec4ba01aff712b3626321e73a
Maxime Antoine <maxime.antoine2@gmail.com>
2015-03-18 18:22:43 +0800

    Added support for custom tab names Added support for formatting of model properties Added support for links when tab names include spaces Added an example using custom tab names

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace SampleConsoleApp
     {
         static void Main(string[] args)
         {
+            //Example 1 - Tab name from class annotation
             var data = new List<IEnumerable<object>>();
 
             using (var db = new SampleDataContext())
@@ -23,6 +25,19 @@ namespace SampleConsoleApp
 
             var bytes = Spreadsheet.Create(data);
             File.WriteAllBytes("MySpreadsheet.xlsx", bytes);
+
+
+            //Example 2 - With custom tab names
+            var data2 = new Dictionary<string, IEnumerable<object>>();
+
+            using (var db = new SampleDataContext())
+            {
+                data2.Add("Tab 1", db.Users.OrderBy(x => x.Name).ToList());
+                data2.Add("Tab 2", db.Orders2.OrderBy(x => x.Customer).ThenByDescending(x => x.Date).ToList());
+            }
+
+            var bytes2 = Spreadsheet.Create(data2);
+            File.WriteAllBytes("MySpreadsheet2.xlsx", bytes2);
         }
     }
 
@@ -43,15 +58,34 @@ namespace SampleConsoleApp
     [DisplayName("Orders")]
     public class Order
     {
-        public int Number { get; set; }
+        public int? Number { get; set; }
 
         public string Item { get; set; }
 
-        [SpreadsheetLink("Customer", "Name")]
+        [SpreadsheetLink("Customers", "Name")]
         public string Customer { get; set; }
 
-        public decimal Price { get; set; }
+        [DisplayFormat(DataFormatString="{0:0.000}")]
+        public decimal? Price { get; set; }
 
-        public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString="{0:dd/MM/yyyy}")]
+        public DateTime? Date { get; set; }
+    }
+
+    [DisplayName("Orders")]
+    public class Order2
+    {
+        public int? Number { get; set; }
+
+        public string Item { get; set; }
+
+        [SpreadsheetLink("Tab 1", "Name")]
+        public string Customer { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:0.000}")]
+        public decimal? Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? Date { get; set; }
     }
 }
